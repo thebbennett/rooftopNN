14168d5406f9bddaf3a5f727b44d5c051d7b904a
thebbennett <thebrittanybennett@gmail.com>
2018-12-25 20:21:08 -0700

    merry christmas this is a non noise result

--- model.py
+++ model.py
@@ -24,11 +24,15 @@ from keras.layers.core import Dropout
 from keras.preprocessing import image
 from tf_unet import unet, util, image_util
 from PIL import Image
+from keras import optimizers
+from keras import regularizers
+from keras import initializers
+from keras.callbacks import EarlyStopping
 
+#callbacks = [history, EarlyStopping(monitor='val_loss', patience=5, verbose=1, min_delta=1e-4)]
 
-
-os.chdir("/home/thebbennett/rooftopNN/Images/compressed/images/")
-
+os.chdir("/home/thebbennett/rooftopNN/data/")
+np.random.seed(1234)
 class DataSeq(Sequence):
     def __init__(self,batch_size,crop_size,img_name_list):
         self.batch_size = batch_size
@@ -37,106 +41,128 @@ class DataSeq(Sequence):
     
     def __len__(self,):
         return len(img_name_list)//self.batch_size
-    
     def __getitem__(self,idx):
-        
-        img_names = np.random.choice(self.img_name_list,self.batch_size,replace=False)
-        mask_names = [i.replace('train/','valid/') for i in img_names]        
-        imgs = [PIL.Image.open(img_name) for img_name in img_names]
-        masks = [PIL.Image.open(mask_name) for mask_name in mask_names]
-        
-        limit_left = 0
-        img = imgs[0]
-        limit_right = img.size[0] - self.crop_size[0]
-        limit_bottom = 0
-        limit_top = img.size[1] - self.crop_size[1]
-        
-        x_array_list = []
-        y_array_list = []
-        for img,mask in zip(imgs,masks):
-            x = np.random.uniform(limit_left,limit_right)
-            y = np.random.uniform(limit_bottom,limit_top)
-            crop_coordinate_tuple = (x,y,x+self.crop_size[0],y+self.crop_size[1])
-            img_crop = img.crop(crop_coordinate_tuple)
-            mask_crop = mask.crop(crop_coordinate_tuple)
-            if random.randint(0,100) < 50:
-                img_crop = np.flip(img_crop)
-                mask_crop = np.flip(mask_crop)
-            if random.randint(0,100) < 50:
-                img_crop = np.rot90(img_crop)
-                mask_crop = np.rot90(mask_crop)
-            x_array_list.append( np.array(img_crop) )
-            y_array_list.append( np.array(mask_crop) )
-        batch_y = np.array(y_array_list) / 255
-        batch_x = np.array(x_array_list) / 255
-    
+            img_names = np.random.choice(self.img_name_list,self.batch_size,replace=False)
+            mask_names = [i.replace('images/','labels/') for i in img_names]        
+            imgs = [PIL.Image.open(img_name) for img_name in img_names]
+            masks = [PIL.Image.open(mask_name) for mask_name in mask_names]
+            center = (random.randint((800-40)/2,(800-40)/2),random.randint((800-40)/2,(800-40)/2))
+            limit_left = 0
+            img = imgs[0]
+            limit_right = img.size[0] - self.crop_size[0]
+            limit_bottom = 0
+            limit_top = img.size[1] - self.crop_size[1]
 
-        
-        return batch_x, batch_y[:,:,:,:1]
-    
+            x_array_list = []
+            y_array_list = []
+            for img,mask in zip(imgs,masks):
+                x = np.random.uniform(limit_left,limit_right)
+                y = np.random.uniform(limit_bottom,limit_top)
+                
+                params = np.random.uniform(0,360)
+                img = img.rotate(params,center=center)
+                mask = mask.rotate(params,center=center)
+
+                crop_coordinate_tuple = (x,y,x+self.crop_size[0],y+self.crop_size[1])
+                img_crop = img.crop(crop_coordinate_tuple)
+                mask_crop = mask.crop(crop_coordinate_tuple)
+                img_crop = np.array(img_crop)
+                mask_crop = np.array(mask_crop)
+                if random.randint(0,100) < 50:
+                    img_crop = np.flip(img_crop)
+                    mask_crop = np.flip(mask_crop)
+                if random.randint(0,100) < 50:
+                    img_crop = np.rot90(img_crop)
+                    mask_crop = np.rot90(mask_crop)
+
+                x_array_list.append(img_crop) 
+                y_array_list.append(mask_crop) 
+            batch_y = np.array(y_array_list) / 255
+            batch_x = np.array(x_array_list) / 255
 
 
-path = 'train/*.jpg'   
+
+            return batch_x, batch_y[:,:,:,:1]
+
+
+path = 'train/images/*.jpg'   
 img_name_list=glob.glob(path)
 
-test = DataSeq(batch_size = 20, crop_size = (256,256),img_name_list = img_name_list)
-batch_x, batch_y = test[0]
+train = DataSeq(batch_size = 15, crop_size = (256,256),img_name_list = img_name_list)
+batch_x, batch_y = train[0]
+
+path = 'test/images/*.jpg'   
+img_name_list=glob.glob(path)
+test = DataSeq(batch_size = 5, crop_size = (256,256),img_name_list = img_name_list)
+test_x, test_y = test[0]
 
 #create a convultional kernel. output 4 filters , length of 3, 
 #have output the same size as the input
 #use relu activation 
 
+# , kernel_initializer= init, bias_initializer='zeros'
+# kernel_regularizer=regularizers.l2(0.01)
+
 inputs = Input(shape=(256,256,3))
 x = inputs
 
-x = Conv2D(4,kernel_size=3,strides=2,padding='same',activation='elu')(x)
-x = Conv2D(8,kernel_size=3,strides=1,padding='same',activation='elu')(x)
-x = Conv2D(12,kernel_size=3,strides=2,padding='same',activation='elu')(x)
+x = Conv2D(2,kernel_size=3,strides=2,padding='same',activation='elu')(x)
+x = Conv2D(4,kernel_size=3,strides=1,padding='same',activation='elu')(x)#x = Conv2D(12,kernel_size=3,strides=2,padding='same',activation='elu',  kernel_regularizer=regularizers.l2(0.01))(x)
+#x = Conv2D(8,kernel_size=3,strides=2,padding='same',activation='elu')(x)
+"""
 x = Conv2D(16,kernel_size=3,strides=1,padding='same',activation='elu')(x)
 x = Conv2D(24,kernel_size=3,strides=2,padding='same',activation='elu')(x)
 x = Conv2D(32,kernel_size=3,strides=1,padding='same',activation='elu')(x)
 x = Conv2D(40,kernel_size=3,strides=2,padding='same',activation='elu')(x)
-x = Conv2D(28,kernel_size=3,strides=1,padding='same',activation='elu')(x)
-x = Conv2D(30,kernel_size=3,strides=2,padding='same',activation='elu')(x)
-x = Conv2D(32,kernel_size=3,strides=1,padding='same',activation='elu')(x)
-x = Conv2D(38,kernel_size=3,strides=2,padding='same',activation='elu')(x)
-x = Conv2D(46,kernel_size=3,strides=1,padding='same',activation='elu')(x)
+x = Conv2D(44,kernel_size=3,strides=1,padding='same',activation='elu')(x)
+x = Conv2D(48,kernel_size=3,strides=2,padding='same',activation='elu')(x)
+x = Conv2D(52,kernel_size=3,strides=1,padding='same',activation='elu')(x)
+x = Conv2D(56,kernel_size=3,strides=2,padding='same',activation='elu')(x)
+x = Conv2D(60,kernel_size=3,strides=1,padding='same',activation='elu')(x)
 
 
-x = Conv2DTranspose(46,kernel_size=3,strides=1,padding='same',activation='elu')(x)
-x = Conv2DTranspose(38,kernel_size=3,strides=2,padding='same',activation='elu')(x)
-x = Conv2DTranspose(32,kernel_size=3,strides=1,padding='same',activation='elu')(x)
-x = Conv2DTranspose(30,kernel_size=3,strides=2,padding='same',activation='elu')(x)
-x = Conv2DTranspose(28,kernel_size=3,strides=1,padding='same',activation='elu')(x)
+x = Conv2DTranspose(60,kernel_size=3,strides=1,padding='same',activation='elu')(x)
+x = Conv2DTranspose(56,kernel_size=3,strides=2,padding='same',activation='elu')(x)
+x = Conv2DTranspose(52,kernel_size=3,strides=1,padding='same',activation='elu')(x)
+x = Conv2DTranspose(48,kernel_size=3,strides=2,padding='same',activation='elu')(x)
+x = Conv2DTranspose(44,kernel_size=3,strides=1,padding='same',activation='elu')(x)
 x = Conv2DTranspose(40,kernel_size=3,strides=2,padding='same',activation='elu')(x)
 x = Conv2DTranspose(32,kernel_size=3,strides=1,padding='same',activation='elu')(x)
 x = Conv2DTranspose(24,kernel_size=3,strides=2,padding='same',activation='elu')(x)
 x = Conv2DTranspose(16,kernel_size=3,strides=1,padding='same',activation='elu')(x)
-x = Conv2DTranspose(12,kernel_size=3,strides=2,padding='same',activation='elu')(x)
-x = Conv2DTranspose(8,kernel_size=3,strides=1,padding='same',activation='elu')(x)
-x = Conv2DTranspose(4,kernel_size=3,strides=2,padding='same',activation='elu')(x)
+"""
+#x = Conv2DTranspose(8,kernel_size=3,strides=2,padding='same',activation='elu')(x)
+x = Conv2DTranspose(4,kernel_size=3,strides=1,padding='same',activation='elu')(x)
+x = Conv2DTranspose(2,kernel_size=3,strides=2,padding='same',activation='elu')(x)
 x = Conv2DTranspose(1,kernel_size=3,strides=1,padding='same',activation='sigmoid')(x)
+#x = Conv2D(1,kernel_size=3,strides=1,padding='same',activation='relu')(x)
+#x = Conv2D(1,kernel_size=3,strides=1,padding='same',activation='sigmoid')(x)
 
 model = Model(inputs,x)
 
+#from keras.optimizers import SGD
 
-
-model.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy','mae'])
+adam = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(optimizer= adam ,loss='binary_crossentropy', metrics=['accuracy','mae'])
 model.summary()
 
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(batch_x, batch_y[:,:,:,:1],test_size=0.2)
+#from sklearn.model_selection import train_test_splitm#X_train, X_test, y_train, y_test = train_test_split(batch_x, batch_y[:,:,:,:1],test_size=0.2)
                    
-history = model.fit_generator(generator = test,
+history = model.fit_generator(generator = train,
                    steps_per_epoch = 20,
-                   epochs = 5,
-                   validation_data=(X_test, y_test), 
-                   validation_steps=None)
+                   epochs = 10,
+                   #callbacks=callbacks, # Early stopping
+                   validation_data = test,            
+                   validation_steps= 5)
+                   #verbose = 0)
 
 print(history.history.keys())
 
 import matplotlib.pyplot as plt
+
+os.chdir("/home/thebbennett/rooftopNN/plots")
+
 
 # summarize history for loss
 f1 = plt.figure(figsize=(10, 10))
@@ -169,17 +195,15 @@ plt.show()
 f3.savefig("accuracy.png")
 
 
-img =image.load_img('/home/thebbennett/rooftopNN/Images/compressed/images/train/036610ne239nw_compressed_resized.jpg',
+img =image.load_img('/home/thebbennett/rooftopNN/data/train/images/036622ne225sw_compressed_resized.jpg',
                     target_size = (256,256))
-plt.imshow(img)
-
 test_image = image.img_to_array(img)
 
 test_image = np.expand_dims(test_image, axis=0)
 predicted_mask_batch = model.predict(test_image)
 
 predicted_mask = predicted_mask_batch[0]
-predicted_mask = predicted_mask.squeeze()
+predicted_mask1 = predicted_mask.squeeze()
 f4 = plt.figure(figsize=(30, 15))
 ax1 = f4.add_subplot(1,2, 1)
 ax1.set_title("Original Image",fontdict={'fontsize': 60, 'fontweight': 'medium'})
@@ -187,7 +211,35 @@ plt.imshow(img)
 ax2 = f4.add_subplot(1,2, 2)
 ax2.set_title("Predicted Mask",fontdict={'fontsize': 60, 'fontweight': 'medium'})
 plt.imshow(img)
-plt.imshow(predicted_mask, alpha=0.6)
+plt.imshow(predicted_mask1 > 0.5, alpha = 0.6)
 plt.show(block=True)
 f4.savefig("predict.png")
 
+
+
+
+img =image.load_img('/home/thebbennett/rooftopNN/data/train/images/046733se120sw_compressed_resized.jpg',
+                    target_size = (256,256))
+test_image = image.img_to_array(img)
+
+test_image = np.expand_dims(test_image, axis=0)
+predicted_mask_batch = model.predict(test_image)
+
+predicted_mask = predicted_mask_batch[0]
+predicted_mask2 = predicted_mask.squeeze()
+f5 = plt.figure(figsize=(30, 15))
+ax1 = f5.add_subplot(1,3, 1)
+ax1.set_title("Original Image",fontdict={'fontsize': 60, 'fontweight': 'medium'})
+plt.imshow(img)
+ax2 = f5.add_subplot(1,3, 2)
+ax2.set_title("Predicted Mask",fontdict={'fontsize': 60, 'fontweight': 'medium'})
+plt.imshow(img)
+plt.imshow(predicted_mask2 > 0.5, alpha = 0.6)
+ax3 = f5.add_subplot(1,3,3)
+ax3.set_title("Probabilities",fontdict={'fontsize': 60, 'fontweight': 'medium'})
+plt.imshow(predicted_mask2)
+
+plt.show(block=True)
+f5.savefig("predict2.png")
+
+print(model.get_weights())
